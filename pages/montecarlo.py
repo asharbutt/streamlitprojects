48957f7e8eca3871371825c63514228568938ce6
asharbutt <49199106+asharbutt@users.noreply.github.com>
2026-04-02 10:05:05 +0100

    Update montecarlo.py

--- pages/montecarlo.py
+++ pages/montecarlo.py
@@ -11,7 +11,7 @@ import streamlit as st
 import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import vanilla_mc_sim as mc
+from utils import vanilla_mc_sim as mc
 
 
 # ── Page config ──
