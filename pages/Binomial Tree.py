e4719cb334e8f5d82e452e1a08ee5b0dbf2989b6
asharbutt <49199106+asharbutt@users.noreply.github.com>
2026-04-02 14:09:01 +0100

    Update Binomial Tree.py

--- pages/Binomial Tree.py
+++ pages/Binomial Tree.py
@@ -34,7 +34,7 @@ option_type = st.sidebar.radio("Option Type", ["call", "put"])
 exercise_type = st.sidebar.radio("Option Type", ["European", "American"])
 
 
-p = bt.binomial_tree_vanilla(steps,vol,S,K,T, option_type,r)
+p = bt.binomial_tree_vanilla(steps,vol,S,K,T, option_type,r, exercise_type)
 option_price = p.run_tree()
 st.write("The option price is: ", f"{option_price:.4f}")
 fig = bt.plot_binomial_tree(p)
