be2dcb3db6a913e74acd64d54beaf7282ac9ca33
asharbutt <49199106+asharbutt@users.noreply.github.com>
2026-04-02 09:58:07 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Quant Finance Toolkit")
+st.markdown("""
+A collection of quantitative finance tools built in Python.
+
+**Tools:**
+- Black-Scholes options pricer
+- Monte Carlo simulation
+- More coming soon...
+""")
