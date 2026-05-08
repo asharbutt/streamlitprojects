cedc19c694b47f3a9ed7360d32ca7d697ad1a854
asharbutt <49199106+asharbutt@users.noreply.github.com>
2026-05-08 17:21:15 +0100

    Update bs.py

--- utils/bs.py
+++ utils/bs.py
@@ -2,14 +2,14 @@ import numpy as np
 from scipy.stats import norm
 
 def bs_call(S, K, vol, T, r, q):
-    d_1 = (np.log(S / K) + (r - q + 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
+    d_1 = (np.log(S / K) + (r - q + 0.5 * vol *vol) * T) / (vol * np.sqrt(T))
     d_2 = d_1 - vol * np.sqrt(T)
-    return S * norm.cdf(d_1) - K * np.exp(-r * T) * norm.cdf(d_2)
+    return S * np.exp(-q*T) norm.cdf(d_1) - K * np.exp(-r * T) * norm.cdf(d_2)
 
 def bs_put(S, K, vol, T, r, q):
     d_1 = (np.log(S / K) + (r - q + 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
     d_2 = d_1 - vol * np.sqrt(T)
-    return -S * norm.cdf(-d_1) + K * np.exp(-r * T) * norm.cdf(-d_2)
+    return -S * np.exp(-q*T)*norm.cdf(-d_1) + K * np.exp(-r * T) * norm.cdf(-d_2)
 
 def bs_call_delta(S, K, vol, T, r, q):
     d_1 = (np.log(S / K) + (r - q + 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
